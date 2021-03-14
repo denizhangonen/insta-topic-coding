c3970438bb6a36963a72bee5bd13e424f88fb3e8
Denizhan Gonen <denizhangonen@users.noreply.github.com>
2021-03-14 15:50:43 +0300

    post 2 content

---
+++ post2/index.js
@@ -0,0 +1,58 @@
+// source: https://www.w3schools.com/jsref/obj_console.asp
+
+/* 
+    1.
+    HTTP Status
+    HTTP "Status" Kodları
+    ### Görseller ###
+*/
+
+/* 
+    2. [Blue Background]
+    1xx - Bilgi
+    
+    100 - Devam Edilebilir
+    101 - Protokol Değişikliği
+    103 - İşleniyor
+*/
+
+/* 
+  3. [Green Background]
+  2xx - Başarılı
+
+  200 - Başarılı
+  201 - Yeni Kayıt
+  203 - Kabul Edildi - İşleniyor
+  204 - Başarılı - İçerik Yok
+
+ */
+
+/* 
+  4. [Yello Background]
+  3xx - Yönlendirme
+
+  301 - Kalıcı Olarak Taşındı
+  302 - Bulundu - Geçici Yeni Adres
+  304 - Değişiklik Yok
+ */
+
+/* 
+  5. [Red Background]
+  4xx - Hata
+
+  400 - Hatalı İstek Yapısı 
+  401 - Yetki Hatası (Alırım senden tüm yetkimi - AA)
+  402 - Ödeme Gerekliliği
+  403 - Yasak İstek
+  404 - Bulunamadı
+ */
+
+/* 
+  6. [Orange Background]
+  5xx - Server Error
+
+  500 - Server Hatası
+  502 - Geçersiz Ağ Geçidi
+  503 - Server Hizmet Vermiyor  
+  
+ */
